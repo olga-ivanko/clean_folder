cdb0521ba89c6f92e5cb1ec5cee2208476315b91
olga-ivanko <o.ivanko@me.com>
2023-11-02 10:48:16 +0200

    commit2

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     name="clean_folder",
     version="0.1.0",
-    entry_points={"console_scripts": ["clean_folder=clean_folder.clean:main"]}
-
-)
+    description="folder_sorter_program",
+    author="GoIT_student_Olga_Ivanko",
+    url="https://github.com/olga-ivanko/clean_folder",
+    license="MIT",
+    entry_points={"console_scripts": [
+        "clean_folder=clean_folder.clean:main"]})
